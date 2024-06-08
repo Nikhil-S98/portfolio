54a6005c3933a3f8d2d53326bd20efd18fcc4abe
Nikhil-S98 <124851112+Nikhil-S98@users.noreply.github.com>
2024-06-08 19:09:13 -0400

    Created transitions between pages and hover effect on header name

---
+++ js/main.js
@@ -0,0 +1,26 @@
+import barba from "@barba/core";
+import gsap from "gsap";
+
+barba.init({
+  transitions: [{
+    name: 'opacity-transition',
+    leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 0,
+        duration: 0.5,
+        onComplete() {
+          data.current.container.style.display = 'none';
+        }
+      });
+    },
+    enter(data) {
+      data.next.container.style.display = 'none';
+      gsap.set(data.next.container, { opacity: 0 });
+      data.next.container.style.display = '';
+      return gsap.to(data.next.container, {
+        opacity: 1,
+        duration: 0.5
+      });
+    }
+  }]
+});
